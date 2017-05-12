af50b082201206edcdfd0f53a3bb4db7316c0596
Artem Chernyaev <askanio234@gmail.com>
2017-05-12 21:05:24 +0300

    Enforce DRY principle, parametrised everything, moved bunch of constants to the head of the script

--- cinemas.py
+++ cinemas.py
@@ -7,19 +7,33 @@ AFISHA_SCHEDULE = "http://www.afisha.ru/msk/schedule_cinema/"
 
 NUMBER_OF_MOVIES = 10
 
-MIN_NUMBER_OF_THEATERS = 10
-
-PROXY = "183.88.185.181:8080"
+MIN_NUMBER_OF_THEATERS = 80
 
 KINOPOISK = "https://www.kinopoisk.ru/index.php"
 
-def fetch_afisha_page(url):
-    request = requests.get(url)
+PARAMETERS = dict()
+
+PARAMETERS["payload"] = {"first": "yes", "kp_query": ""}
+
+PARAMETERS["headers"] = {"User-Agent": "Mozilla/5.0"
+"(Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0"}
+
+
+def get_raw_html(request):
     if request.status_code == 200:
         request.encoding = "utf-8"
         return request.text
+
+
+def fetch_page(url, parameters=None):
+    if parameters is None:
+        request = requests.get(url)
+        return get_raw_html(request)
     else:
-        print("Нет ответа от сервера")
+        payload = parameters["payload"]
+        headers = parameters["headers"]
+        request = requests.get(url, params=payload, headers=headers)
+        return get_raw_html(request)
 
 
 def parse_afisha_list(raw_html):
@@ -46,32 +60,18 @@ def get_movie_rating(raw_html):
         movie_rating = default
     else:
         movie_rating = float(movie_ratings.text)
-        
+
     return movie_rating
 
 
 def add_movie_rating(movies_list):
     movies_info = dict()
     for movie in movies_list:
+        PARAMETERS["payload"]["kp_query"] = movie
         movies_info[movie] = (get_movie_rating(
-                            fetch_kinopoisk_page(movie)))
+                            fetch_page(KINOPOISK, PARAMETERS)))
         time.sleep(random.randrange(5, 10))
     return movies_info
-
-
-def fetch_kinopoisk_page(movie_title):
-    url = KINOPOISK
-    payload = {"first": "yes", "kp_query": movie_title}
-    headers = {"User-Agent": "Mozilla/5.0"
-    "(Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0"}
-    proxy = {"http": PROXY, }
-    request = requests.get(url, params=payload, headers=headers,
-        proxies=proxy)
-    if request.status_code == 200:
-        request.encoding = "utf-8"
-        return request.text
-    else:
-        print("{}, нет ответа от кинопоиска".format(movie_title))
 
 
 def sort_movies_based_on_rating(movie_dict):
@@ -85,7 +85,7 @@ def output_movies_to_console(movies):
 
 
 if __name__ == '__main__':
-    movies = parse_afisha_list(fetch_afisha_page(AFISHA_SCHEDULE))
+    movies = parse_afisha_list(fetch_page(AFISHA_SCHEDULE))
     movies_filtered = (filter_movies(movies, MIN_NUMBER_OF_THEATERS))
     movies_rated = add_movie_rating(movies_filtered)
     movies_sorted = sort_movies_based_on_rating(movies_rated)
